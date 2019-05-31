5e90269ff546009d8377e68d6914949795a06f2f
yintaobtc <yintao@protonmail.com>
2019-05-31 02:59:00 +0200

    deploy heroku without node

--- donatecrw/settings.py
+++ donatecrw/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = "1239021i3mdapsdsa9adas"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'social.apps.SocialConfig',
     'services.apps.ServicesConfig',
     'qr_code',
-    'django_heroku.settings(locals())',
+    #'django_heroku.settings(locals())',
 ]
 
 MIDDLEWARE = [
