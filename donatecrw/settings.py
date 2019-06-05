9bb6ec233d993804759873a18fef642d70d756fe
yintaobtc <yintao@protonmail.com>
2019-06-05 17:15:35 +0200

    Debug True Static

--- donatecrw/settings.py
+++ donatecrw/settings.py
@@ -125,12 +125,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/core/static/'
+"""
 if DEBUG:
    STATICFILES_DIRS = [
    os.path.join(BASE_DIR, 'core/static'),
    ]
 else:
-   STATIC_ROOT = os.path.join(BASE_DIR,'core/static')
+"""
+STATIC_ROOT = os.path.join(BASE_DIR,'core/static')
 
 
 #Media Config
