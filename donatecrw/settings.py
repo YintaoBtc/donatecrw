7f087a4e1411a8ee051f3b950b89d0c973e73634
yintaobtc <yintao@protonmail.com>
2019-06-07 17:29:22 +0200

    Deploy Heroku

--- donatecrw/settings.py
+++ donatecrw/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")  
+SECRET_KEY = os.environ("SECRET_KEY")  
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -82,12 +82,12 @@ WSGI_APPLICATION = 'donatecrw.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv("DB_ENGINE"),
-        'NAME':  os.getenv("DB_NAME"),                      
-        'USER':  os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST':  os.getenv("DB_HOST"),
-        'PORT':  os.getenv("DB_PORT"),
+        'ENGINE': os.environ("DB_ENGINE"),
+        'NAME':  os.environ("DB_NAME"),                      
+        'USER':  os.environ("DB_USER"),
+        'PASSWORD': os.environ("DB_PASSWORD"),
+        'HOST':  os.environ("DB_HOST"),
+        'PORT':  os.environ("DB_PORT"),
     }
 }
 
